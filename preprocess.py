870ec9aa1fb179b38de6ad366bc3141857dd7d3c
lgkpba <lgkpba@gmail.com>
2026-06-28 14:58:08 -0300

    ajustado para ser compatível com a estrutura do README

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,11 @@ import os
 os.makedirs('data', exist_ok=True)
 
 # Carregar metadados
-movies = pd.read_csv('data/MovieSummaries/movie.metadata.tsv', sep='\t', header=None,
+movies = pd.read_csv('data/movie.metadata.tsv', sep='\t', header=None,
     usecols=[0, 2], names=['movie_id', 'title'])
 
 # Carregar sinopses
-summaries = pd.read_csv('data/MovieSummaries/plot_summaries.txt', sep='\t', header=None,
+summaries = pd.read_csv('data/plot_summaries.txt', sep='\t', header=None,
     names=['movie_id', 'summary'])
 
 # Unir
